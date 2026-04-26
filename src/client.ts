001a66bce31cb22005f5abf30a5305f1522741f8
0xVersion <dev@0xVersion.com>
2026-04-26 15:30:49 +0300

    Add system program to transaction and introduce transfer_referral instruction

--- src/client.ts
+++ src/client.ts
@@ -1382,6 +1382,7 @@ export class OrbsGameSDK {
         rootAccount,
         roundPage: roundPagePda,
         authority: authority.publicKey,
+        systemProgram: PROGRAM_ADDRESSES.SYSTEM_PROGRAM,
       })
       .remainingAccounts(remainingAccounts)
       .transaction();
